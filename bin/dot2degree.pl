2152728bb3512e4bc043b9fbed467cb977591d58
Sergio Castillo <s.cast.lara@gmail.com>
2015-12-18 09:29:55 +0100

    Now reads tabular graphs

--- bin/dot2degree.pl
+++ bin/dot2degree.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 use Getopt::Long;
 use Dot::Parser qw(parse_dot);
+use Tabgraph::Reader qw(read_tabgraph);
 use Graphs::Degree qw(nodes_by_degree);
 
 # GET OPTIONS
@@ -11,26 +12,32 @@ my %options;
 $options{rule} = "TOTAL"; # default
 
 GetOptions (
-    \%options, 
+    \%options,
     "input=s",
     "output=s",
-    "rule=s"   
- ); 
+    "rule=s"
+ );
 
 
 # CHECK COMMAND-LINE ARGUMENTS
 if (not defined $options{input}) {
-    die "You have to give me an input DOT file\nUse option -i\n";
+    die "You have to give me an input graph file\nUse option -i\n";
 } elsif (not defined $options{output}) {
     die "You have to give me an output file name\nUse option -o\n";
-} 
+}
 
 print STDERR "Using $options{rule} degree to sort the nodes.\n",
              "You can change this behaviour use the option -r\n",
              "\t(choose IN for in-degree or OUT for out-degree)\n";
-             
+
 # MAIN SCRIPT
-my $graph = parse_dot( $options{input} );
+my $graph;
+
+if ($options{input} =~ /\.(dot|gv)$/) {
+    $graph = parse_dot( $options{input} );
+} else {
+    $graph = read_tabgraph( $options{input} );
+}
 
 my $options_to_sort = {
     graph => $graph,
